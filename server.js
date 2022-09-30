a7f6d4119223739873edc58f566c7433faa036d2
Biswajit Sahoo <sahoosipun834@gmail.com>
2022-10-01 00:01:12 +0530

    set the port

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose.connect(DB, {
 // console.log(process.env.NODE_ENV)
 //console.log(process.env)
 
-const port = 3000
+const port =process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log('server is waiting........')
 })
